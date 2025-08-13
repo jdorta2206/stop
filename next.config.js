96744cf0f9e29fad5a308b8f45ea0c2f9cb49d00
Jaime Dorta <jdorta2206@gmail.com>
2025-08-13 07:55:56 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.js
+++ next.config.js
@@ -2,7 +2,14 @@
 const nextConfig = {
     images: {
         unoptimized: true
-    }
+    },
+    webpack: (config, { isServer }) => {
+        // Añadir un alias para el paquete 'process' para que funcione en el navegador
+        if (!isServer) {
+            config.resolve.alias['process'] = 'process/browser';
+        }
+        return config;
+    },
 };
 
 export default nextConfig;
