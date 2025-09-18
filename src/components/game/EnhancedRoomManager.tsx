7580335afab55d97f6848609de72631ffc0d2be3
jaime <todocuentasnhdo@gmail.com>
2025-09-19 00:54:00 +0200

    1

--- src/components/game/EnhancedRoomManager.tsx
+++ src/components/game/EnhancedRoomManager.tsx
@@ -9,7 +9,7 @@ import { Label } from '@/components/ui/label';
 import { Switch } from '@/components/ui/switch';
 import { 
   Users, 
-  Settings, 
+  Settings, _marker
   Share2, 
   Lock, 
   Unlock,
