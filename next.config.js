8fc9a205e6371ea6c94b9747b0406e376c2d8153
Jaime Dorta <jdorta2206@gmail.com>
2025-09-20 12:09:20 +0000

    Try fixing this error: `Unhandled Runtime Error: TypeError: Cannot read

--- next.config.js
+++ next.config.js
@@ -2,13 +2,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    webpack: (config, { isServer }) => {
-        // Añadir un alias para el paquete 'process' para que funcione en el navegador
-        if (!isServer) {
-            config.resolve.alias['process'] = 'process/browser';
-        }
-        return config;
-    }
 };
 
 export default nextConfig;
