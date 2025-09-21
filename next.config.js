18f4fe6ede98b5e4a5e4f30ea106edf266b06ae9
Jaime Dorta <jdorta2206@gmail.com>
2025-09-21 13:13:26 +0000

    al entrar en sala privada no entra y da estos errores

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
+  // Se desactiva cualquier configuración PWA para evitar la generación de Service Workers
+  // que puedan interferir con las peticiones a la base de datos de Firebase.
 };
 
 export default nextConfig;
