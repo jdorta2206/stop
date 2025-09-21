61aa07916405ad0d3e5c37ae72ea11df45978c9c
jaime <todocuentasnhdo@gmail.com>
2025-09-22 00:04:40 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,27 +3,12 @@
 import './globals.css';
 import { Toaster } from "@/components/ui/sonner";
 import { Providers } from '@/components/providers/Providers';
-import { useEffect } from 'react';
-
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
-  useEffect(() => {
-    // Eliminar cualquier Service Worker activo para evitar conflictos de red con Firebase.
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.getRegistrations().then(function(registrations) {
-        for(let registration of registrations) {
-          registration.unregister();
-        }
-      }).catch(function(err) {
-        console.log('Service Worker unregistration failed: ', err);
-      });
-    }
-  }, []);
 
   return (
     <html lang="es" suppressHydrationWarning className="h-full">
