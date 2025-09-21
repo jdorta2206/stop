5ae5804c17d11e6f3a9e2983a771fe6f133765e2
jaime <jdorta2206@gmail.com>
2025-09-22 01:23:34 +0200

    1

--- next.config.js
+++ next.config.js
@@ -1,13 +1,1 @@
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  output: 'export',
-  // Desactiva explícitamente la generación de Service Workers
-  // para evitar conflictos con las peticiones de red de Firebase.
-  experimental: {
-    serviceWorker: false,
-  },
-};
-
-export default nextConfig;
