0b752e1ae30937905d6c064cce21f00524cf416b
Jaime Dorta <jdorta2206@gmail.com>
2025-09-22 09:17:11 +0000

    AIzaSyB5PJI1Lc0GD4BZoL5EUidzhQQZAh0R7uo

--- src/lib/firebase-config.ts
+++ src/lib/firebase-config.ts
@@ -8,7 +8,7 @@
 export const firebaseConfig = {
   "projectId": "global-stop",
   "appId": "1:902072408470:web:a9b19b24c5e791a84865b7",
-  "apiKey": "AIzaSyDw63q5Hn0TCDIFMggy_YV9PQ-fUvmNDJQ",
+  "apiKey": "AIzaSyB5PJI1Lc0GD4BZoL5EUidzhQQZAh0R7uo",
   "authDomain": "global-stop.firebaseapp.com",
   "measurementId": "G-P41T2BEMKZ",
   "messagingSenderId": "902072408470"
