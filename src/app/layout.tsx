f95d43ec417da9b7b55322acbb54349b02d6847d
Jaime Dorta <jdorta2206@gmail.com>
2025-09-22 00:31:39 +0000

    ya depliego pero no se crea la sala privada: sw.js:85 Uncaught (in promi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,24 @@
 import './globals.css';
 import { Toaster } from "@/components/ui/sonner";
 import { Providers } from '@/components/providers/Providers';
+import { useEffect } from 'react';
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  useEffect(() => {
+    // Force remove any existing service workers.
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.getRegistrations().then(function(registrations) {
+        for(let registration of registrations) {
+          registration.unregister();
+        }
+      });
+    }
+  }, []);
 
   return (
     <html lang="es" suppressHydrationWarning className="h-full">
