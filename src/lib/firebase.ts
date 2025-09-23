4e296f28ace4c42b7775c6d31fe8e546a653e9ff
Jaime Dorta <jdorta2206@gmail.com>
2025-09-23 22:07:13 +0000

    pues no inicia

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,7 +2,6 @@
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, FacebookAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import { initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check';
 import { firebaseConfig } from '@/lib/firebase-config';
 
 // Initialize Firebase App
@@ -10,19 +9,6 @@ import { firebaseConfig } from '@/lib/firebase-config';
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const db = getFirestore(app);
-
-// Initialize App Check
-if (typeof window !== 'undefined') {
-  try {
-    initializeAppCheck(app, {
-      provider: new ReCaptchaV3Provider('6LdOutlrAAAAAHyftbN3PIP8s72soDyqcUsCehiQ'), 
-      isTokenAutoRefreshEnabled: true
-    });
-    console.log("Firebase App Check initialized successfully.");
-  } catch (error) {
-    console.error("Error initializing Firebase App Check:", error);
-  }
-}
 
 // --- Providers ---
 const googleProvider = new GoogleAuthProvider();
