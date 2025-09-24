11ec9b96bd84aa7bc864a342dc8f282807fc9611
Jaime Dorta <jdorta2206@gmail.com>
2025-09-24 14:50:26 +0000

    4:49:11 PM: > Build failed because of webpack errors 4:49:11 PM:  ELIFEC

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,30 +1,20 @@
 
 import { initializeApp, getApps, getApp, type FirebaseApp } from 'firebase/app';
 import { getAuth, type Auth } from 'firebase/auth';
-import { getFirestore, type Firestore } from 'firebase/firestore';
+import { getFirestore, type Firestore }from 'firebase/firestore';
 
 const firebaseConfig = {
-  "apiKey": "AIzaSyDw63q5Hn0TCDIFMggy_YV9PQ-fUvmNDJQ",
-  "authDomain": "global-stop.firebaseapp.com",
-  "projectId": "global-stop",
-  "storageBucket": "stop-game-v2.appspot.com",
-  "messagingSenderId": "902072408470",
-  "appId": "1:902072408470:web:a9b19b24c5e791a84865b7",
-  "measurementId": "G-P41T2BEMKZ"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: "global-stop.firebaseapp.com",
+  projectId: "global-stop",
+  storageBucket: "stop-game-v2.appspot.com",
+  messagingSenderId: "902072408470",
+  appId: "1:902072408470:web:a9b19b24c5e791a84865b7",
+  measurementId: "G-P41T2BEMKZ"
 };
 
-// Patrón Singleton para asegurar que Firebase solo se inicialice una vez.
-let app: FirebaseApp;
-let auth: Auth;
-let db: Firestore;
-
-if (getApps().length === 0) {
-  app = initializeApp(firebaseConfig);
-} else {
-  app = getApp();
-}
-
-auth = getAuth(app);
-db = getFirestore(app);
+const app: FirebaseApp = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
+const auth: Auth = getAuth(app);
+const db: Firestore = getFirestore(app);
 
 export { app, auth, db, firebaseConfig };
