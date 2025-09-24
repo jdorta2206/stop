93731ff1ec2199b0dabdf4e084ab5db211e94e7a
jaime <todocuentasnhdo@gmail.com>
2025-09-24 17:00:03 +0200

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,18 +3,27 @@ import { initializeApp, getApps, getApp, type FirebaseApp } from 'firebase/app';
 import { getAuth, type Auth } from 'firebase/auth';
 import { getFirestore, type Firestore }from 'firebase/firestore';
 
+// Configuración de Firebase para el proyecto stop-game-v2
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "global-stop.firebaseapp.com",
-  projectId: "global-stop",
+  apiKey: "API_KEY",
+  authDomain: "stop-game-v2.firebaseapp.com",
+  projectId: "stop-game-v2",
   storageBucket: "stop-game-v2.appspot.com",
-  messagingSenderId: "902072408470",
-  appId: "1:902072408470:web:a9b19b24c5e791a84865b7",
-  measurementId: "G-P41T2BEMKZ"
+  messagingSenderId: "397399335694",
+  appId: "1:397399335694:web:53d3f448c34f2d338f972b"
 };
 
-const app: FirebaseApp = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
-const auth: Auth = getAuth(app);
-const db: Firestore = getFirestore(app);
+let app: FirebaseApp;
+let auth: Auth;
+let db: Firestore;
+
+if (getApps().length === 0) {
+  app = initializeApp(firebaseConfig);
+} else {
+  app = getApp();
+}
+
+auth = getAuth(app);
+db = getFirestore(app);
 
 export { app, auth, db, firebaseConfig };
