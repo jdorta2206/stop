bae4bb7d104b7729106301646a86a3f3c69d6adf
jaime <todocuentasnhdo@gmail.com>
2025-09-25 11:55:39 +0200

    1

--- src/lib/firebase-config.ts
+++ src/lib/firebase-config.ts
@@ -2,18 +2,17 @@
 import { getApp, getApps, initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration for stop-game-v2
+// The configuration is now read from environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyAJY3Jq9g_NfNnqVO9bY08i9a_3aZa_YjM",
-  authDomain: "stop-game-v2.firebaseapp.com",
-  projectId: "stop-game-v2",
-  storageBucket: "stop-game-v2.appspot.com",
-  messagingSenderId: "668640003802",
-  appId: "1:668640003802:web:a185f56118989cca0c87e3",
-  measurementId: "G-9Q2P2EFT6S"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
