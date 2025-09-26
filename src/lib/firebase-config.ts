5454ff21a7226bab218bbf09acb482ca31227ebb
Jaime Dorta <jdorta2206@gmail.com>
2025-09-26 07:47:39 +0000

    prueba con esta haber

--- src/lib/firebase-config.ts
+++ src/lib/firebase-config.ts
@@ -1,11 +1,11 @@
 
 // Your web app's Firebase configuration for global-stop
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  apiKey: "AIzaSyDw53q5HnBTCDIFMggy_YV9PQ-fUvrNDJQ",
   authDomain: "global-stop.firebaseapp.com",
   projectId: "global-stop",
   storageBucket: "global-stop.appspot.com",
-  messagingSenderId: "277985396554",
-  appId: "1:277985396554:web:485a3c9e95c1b563459c04",
-  measurementId: "G-65Y9J10H4M"
+  messagingSenderId: "902072408470",
+  appId: "1:902072408470:web:a9b19b24c5e791a84865b7",
+  measurementId: "G-P41T2BEMKZ"
 };
