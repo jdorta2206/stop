33f9f02b67e3e87e639cf895f454d51ff66a4b4d
Jaime Dorta <jdorta2206@gmail.com>
2025-09-26 08:10:43 +0000

    hay lo tienes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,6 +4,7 @@ import { getAuth, GoogleAuthProvider, FacebookAuthProvider } from 'firebase/auth
 import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration for global-stop
+// This is the CORRECT configuration for the "Stop" app.
 export const firebaseConfig = {
   apiKey: "AIzaSyC2k78iF5f3c7A9b1e7D5fG3h2j1k9L8M",
   authDomain: "global-stop.firebaseapp.com",
