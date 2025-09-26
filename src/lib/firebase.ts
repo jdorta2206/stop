878cb4cc6d6f71c10d235c75d3fa90af7aa84a82
Jaime Dorta <jdorta2206@gmail.com>
2025-09-26 16:45:53 +0000

    este es el error que da y ese es el proyecto

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,17 +3,16 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, FacebookAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-// Your web app's Firebase configuration for global-stop
-// This is the CORRECT configuration for the "Stop" app.
-export const firebaseConfig = {
-    apiKey: "AIzaSyA1B2C3D4E5F6G7H8I9J0K1L2M3N4O5P6",
-    authDomain: "global-stop.firebaseapp.com",
-    projectId: "global-stop",
-    storageBucket: "global-stop.appspot.com",
-    messagingSenderId: "902072408470",
-    appId: "1:902072408470:web:9db3b3c3c1a3915a12bada",
-    measurementId: "G-DP1DM3R96E"
-  };
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCV3-p-gHkIs-k37M284xar7e_pYjVpYyA",
+  authDomain: "global-stop.firebaseapp.com",
+  projectId: "global-stop",
+  storageBucket: "global-stop.appspot.com",
+  messagingSenderId: "902072408470",
+  appId: "1:902072408470:web:9db3b3c3c1a3915a12bada",
+  measurementId: "G-DP1DM3R96E",
+};
 
 
 // Initialize Firebase App
