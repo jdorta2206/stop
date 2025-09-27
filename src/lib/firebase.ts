c96ac2722ad71806199f92db580c7b0da3de164f
Jaime Dorta <jdorta2206@gmail.com>
2025-09-27 11:58:59 +0000

    lo sigue dando

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,15 +3,15 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, FacebookAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-// Your web app's Firebase configuration
+// Your web app's Firebase configuration is now loaded from environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyCV3-p-gHkIs-k37M284xar7e_pYjVpYyA",
-  authDomain: "global-stop.firebaseapp.com",
-  projectId: "global-stop",
-  storageBucket: "global-stop.appspot.com",
-  messagingSenderId: "902072408470",
-  appId: "1:902072408470:web:9db3b3c3c1a3915a12bada",
-  measurementId: "G-DP1DM3R96E",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 
