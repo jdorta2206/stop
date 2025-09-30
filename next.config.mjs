acb8e5c8417773542224c1005c1c056abbfb3740
jaime <75831652+jdorta2206@users.noreply.github.com>
2025-10-01 00:52:20 +0200

    3

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-        port: '',
-        pathname: '/a/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'platform-lookaside.fbsbx.com',
-        port: '',
-        pathname: '/platform/profilepic/**',
-      },
-      {
-          protocol: 'https',
-          hostname: 'api.dicebear.com',
-          port: '',
-          pathname: '/**',
-      }
-    ],
-  },
 };
 
 export default nextConfig;
