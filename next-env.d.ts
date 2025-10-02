43f4ec0874af164c57d89751da28dcd4a3a2004d
Jaime Dorta <jdorta2206@gmail.com>
2025-10-02 09:06:13 +0000

    mira

--- next-env.d.ts
+++ next-env.d.ts
@@ -4,3 +4,13 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/building-your-application/configuring/typescript for more information.
+
+declare namespace NodeJS {
+    interface ProcessEnv {
+        NEXT_PUBLIC_FIREBASE_API_KEY: string;
+        NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: string;
+        NEXT_PUBLIC_FIREBASE_PROJECT_ID: string;
+        NEXT_PUBLIC_FIREBASE_APP_ID: string;
+        NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: string;
+    }
+}
