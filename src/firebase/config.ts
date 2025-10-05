0805d3d756c0932db2b47576b7e7cf75e5eef0f1
Jaime Dorta <jdorta2206@gmail.com>
2025-10-05 23:08:01 +0000

    esto

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,12 @@
 
 // POR FAVOR, REEMPLAZA ESTO CON LA CONFIGURACIÓN DE TU APP "Stop" DESDE LA CONSOLA DE FIREBASE
 export const firebaseConfig = {
-  apiKey: "AIzaSy...",
-  authDomain: "...",
-  projectId: "...",
-  storageBucket: "...",
-  messagingSenderId: "...",
-  appId: "...",
-  measurementId: "..."
+  apiKey: "AIzaSyDw53q5HnBTCDIFMggy_YV9PQ-fUvmNDJQ",
+  authDomain: "global-stop.firebaseapp.com",
+  databaseURL: "https://global-stop-default-rtdb.firebaseio.com",
+  projectId: "global-stop",
+  storageBucket: "global-stop.firebasestorage.app",
+  messagingSenderId: "902072408470",
+  appId: "1:902072408470:web:843e9f91308bb4ec4865b7",
+  measurementId: "G-DP1DM3R96E"
 };
