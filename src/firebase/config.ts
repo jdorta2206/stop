6487baa5bc1795ae8c36b3841f0f1148a12a5f15
jaime <75831652+jdorta2206@users.noreply.github.com>
2025-10-06 00:41:42 +0200

    1

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,10 @@
+
 export const firebaseConfig = {
-  "projectId": "global-stop",
-  "appId": "1:902072408470:web:a9b19b24c5e791a84865b7",
-  "apiKey": "AIzaSyDw63q5Hn0TCDIFMggy_YV9PQ-fUvmNDJQ",
-  "authDomain": "global-stop.firebaseapp.com",
-  "measurementId": "G-P41T2BEMKZ",
-  "messagingSenderId": "902072408470"
+  apiKey: "AIzaSyDw63q5Hn0TCDIFMggy_YV9PQ-fUvmNDJQ",
+  authDomain: "global-stop.firebaseapp.com",
+  projectId: "global-stop",
+  storageBucket: "global-stop.appspot.com",
+  messagingSenderId: "902072408470",
+  appId: "1:902072408470:web:843e9f91308bb4ec4865b7",
+  measurementId: "G-P41T2BEMKZ"
 };
