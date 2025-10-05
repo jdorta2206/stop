eecaffe3e439f64bbde81b73527ffc084e020aee
Jaime Dorta <jdorta2206@gmail.com>
2025-10-05 23:11:15 +0000

    nada no conecta jope es imposible sacar una app 100% funcional con tigo

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -5,7 +5,7 @@ export const firebaseConfig = {
   authDomain: "global-stop.firebaseapp.com",
   databaseURL: "https://global-stop-default-rtdb.firebaseio.com",
   projectId: "global-stop",
-  storageBucket: "global-stop.firebasestorage.app",
+  storageBucket: "global-stop.appspot.com",
   messagingSenderId: "902072408470",
   appId: "1:902072408470:web:843e9f91308bb4ec4865b7",
   measurementId: "G-DP1DM3R96E"
