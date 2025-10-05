0062474428d76b3ed0e1085496fe48666a932b29
jaime <75831652+jdorta2206@users.noreply.github.com>
2025-10-06 01:19:21 +0200

    1

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,12 +1,11 @@
 
 // POR FAVOR, REEMPLAZA ESTO CON LA CONFIGURACIÓN DE TU APP "Stop" DESDE LA CONSOLA DE FIREBASE
 export const firebaseConfig = {
-  apiKey: "AIzaSyDw53q5HnBTCDIFMggy_YV9PQ-fUvmNDJQ",
-  authDomain: "global-stop.firebaseapp.com",
-  databaseURL: "https://global-stop-default-rtdb.firebaseio.com",
-  projectId: "global-stop",
-  storageBucket: "global-stop.appspot.com",
-  messagingSenderId: "902072408470",
-  appId: "1:902072408470:web:843e9f91308bb4ec4865b7",
+  apiKey: "AIzaSyDqP1TZxE1TBRBq8t0cOYbJg0ySd2n9Z2Q",
+  authDomain: "juego-stop-9a8a8.firebaseapp.com",
+  projectId: "juego-stop-9a8a8",
+  storageBucket: "juego-stop-9a8a8.appspot.com",
+  messagingSenderId: "1097782410993",
+  appId: "1:1097782410993:web:02b07e5f3f3c316719b66c",
   measurementId: "G-DP1DM3R96E"
 };
