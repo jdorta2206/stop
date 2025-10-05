7a7b354ccd3df676c45d44514de6ee08fb09d91b
Jaime Dorta <jdorta2206@gmail.com>
2025-10-05 23:06:14 +0000

    hay una registrada la otra no

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,11 @@
 
+// POR FAVOR, REEMPLAZA ESTO CON LA CONFIGURACIÓN DE TU APP "Stop" DESDE LA CONSOLA DE FIREBASE
 export const firebaseConfig = {
-  apiKey: "AIzaSyDw63q5Hn0TCDIFMggy_YV9PQ-fUvmNDJQ",
-  authDomain: "global-stop.firebaseapp.com",
-  projectId: "global-stop",
-  storageBucket: "global-stop.appspot.com",
-  messagingSenderId: "902072408470",
-  appId: "1:902072408470:web:843e9f91308bb4ec4865b7",
-  measurementId: "G-P41T2BEMKZ"
+  apiKey: "AIzaSy...",
+  authDomain: "...",
+  projectId: "...",
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "...",
+  measurementId: "..."
 };
