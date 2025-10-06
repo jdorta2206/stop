a58e90263d26cab706fafd66e4c76a25a2b78a8f
jaime <75831652+jdorta2206@users.noreply.github.com>
2025-10-06 18:19:09 +0200

    1

--- src/components/providers/Providers.tsx
+++ src/components/providers/Providers.tsx
@@ -18,7 +18,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
       <LanguageProvider>
         <TooltipProvider>
           {children}
-          <Toaster />
+          <Toaster richColors />
         </TooltipProvider>
       </LanguageProvider>
     </ThemeProvider>
