69d475a07d0574c160cb11215dec16c76c31aa61
Jaime Dorta <jdorta2206@gmail.com>
2025-10-07 09:29:54 +0000

    9d0843fd-9b33fffc2b4d17b8.js:1  GET https://identitytoolkit.googleapis.c

--- src/components/auth/auth-status.tsx
+++ src/components/auth/auth-status.tsx
@@ -22,7 +22,7 @@ export function AuthStatus() {
   }
 
   if (user) {
-    return <UserAccount user={user} />;
+    return <UserAccount />;
   }
 
   return (
--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -6,36 +6,34 @@ import { getAuth, type Auth } from 'firebase/auth';
 import { getFirestore, type Firestore } from 'firebase/firestore';
 
 let firebaseApp: FirebaseApp;
-let auth: Auth;
-let firestore: Firestore;
+
+// Asegura que Firebase se inicialice solo una vez.
+if (!getApps().length) {
+  firebaseApp = initializeApp(firebaseConfig);
+} else {
+  firebaseApp = getApp();
+}
+
+const auth = getAuth(firebaseApp);
+const firestore = getFirestore(firebaseApp);
 
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
-  if (!getApps().length) {
-    firebaseApp = initializeApp(firebaseConfig);
-    auth = getAuth(firebaseApp);
-    firestore = getFirestore(firebaseApp);
-  } else {
-    firebaseApp = getApp();
-    auth = getAuth(firebaseApp);
-    firestore = getFirestore(firebaseApp);
-  }
   return { firebaseApp, auth, firestore };
 }
 
-export function getSdks(app: FirebaseApp) {
+export function getSdks() {
   return {
-    firebaseApp: app,
-    auth: getAuth(app),
-    firestore: getFirestore(app)
+    firebaseApp,
+    auth,
+    firestore,
   };
 }
+
 
 export * from './provider';
 export * from './client-provider';
 export * from './firestore/use-collection';
 export * from './firestore/use-doc';
-export * from './non-blocking-updates';
-export * from './non-blocking-login';
 export * from './errors';
 export * from './error-emitter';
