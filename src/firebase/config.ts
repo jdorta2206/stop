d11e4fb9cf603e9bffce21da2412d88c0c3d2e09
Jaime Dorta <jdorta2206@gmail.com>
2025-10-07 09:26:09 +0000

    eh creado esta nueva appp web y las otras klas eh eliminado haber si hac

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,11 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyDw63q5Hn0TCDIFMggy_YV9PQ-fUvmNDJQ",
+  apiKey: "AIzaSyADkW37VDrVxKhnWhKJ04FLkp4kjRWDqpk",
   authDomain: "global-stop.firebaseapp.com",
   databaseURL: "https://global-stop-default-rtdb.firebaseio.com",
   projectId: "global-stop",
   storageBucket: "global-stop.firebasestorage.app",
   messagingSenderId: "902072408470",
-  appId: "1:902072408470:web:a9b19b24c5e791a84865b7",
-  measurementId: "G-P41T2BEMKZ"
+  appId: "1:902072408470:web:bf12e7ddc05e44d44865b7",
+  measurementId: "G-CPLX9NQ8YN"
 };
